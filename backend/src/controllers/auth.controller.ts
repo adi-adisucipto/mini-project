a077b61e4c40e12fa00d65f2cccac0867bef27b1
adi-adisucipto <suciptoadi550@gmail.com>
2025-11-24 13:58:27 +0700

    AvatarUpload

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { verificationLinkService, verifyService } from "../services/auth.service
 import { generateReferralCode } from "../utils/generateReferralCode";
 import { verify } from "jsonwebtoken";
 import { SECRET_KEY } from "../configs/env.config";
+import { createCustomError } from "../utils/customError";
 
 export async function verificationLinkController(
     req: Request,
@@ -24,11 +25,13 @@ export async function verificationLinkController(
 
 export async function verifyController(req:Request, res:Response, next:NextFunction) {
     try {
-        const token = req.params.token
-        console.log(token)
-
-        const decoded = verify(token, SECRET_KEY) as { email: string };
-        const { email } = decoded;
+        const authHeader = req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith("Bearer ")) {
+            throw createCustomError(401, "Unauthorize");
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded = verify(token, SECRET_KEY) as { email: string }
+        const email = decoded.email;
         const file = req.file;
         const { username, password, referrerCode, role } = req.body;
 
---
+++ frontend/src/views/auth/verify/components/AvatarUpload.tsx
@@ -0,0 +1,90 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { User } from 'lucide-react';
+
+interface AvatarUploadProps {
+    avatarValue: File | null;
+    setAvatarValue: (file: File | null) => void;
+}
+
+const AvatarUpload: React.FC<AvatarUploadProps> = ({ avatarValue, setAvatarValue }) => {
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (avatarValue) {
+            const url = URL.createObjectURL(avatarValue);
+            setImageUrl(url);
+            return () => URL.revokeObjectURL(url); 
+        } else {
+            setImageUrl(null);
+        }
+    }, [avatarValue]);
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files ? e.target.files[0] : null;
+
+        if (file && file.type.startsWith('image/')) {
+            setAvatarValue(file);
+        } else {
+            setAvatarValue(null);
+        }
+    }
+
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.currentTarget.classList.remove('border-indigo-500', 'bg-indigo-50');
+        const file = e.dataTransfer.files ? e.dataTransfer.files[0] : null;
+
+        if (file) {
+            handleFileChange({ target: { files: [file] } } as unknown as React.ChangeEvent<HTMLInputElement>);
+        }
+    }
+
+    const openFileDialog = () => {
+        fileInputRef.current?.click();
+    };
+
+    const dragOverClass = (e: React.DragEvent<HTMLDivElement>, add: boolean) => {
+        e.preventDefault();
+        if (add) {
+            e.currentTarget.classList.add('border-indigo-500', 'bg-indigo-50');
+        } else {
+            e.currentTarget.classList.remove('border-indigo-500', 'bg-indigo-50');
+        }
+    };
+
+
+  return (
+    <div
+            className={`w-12 h-12 rounded-full flex flex-col justify-center items-center ${imageUrl ? 'border-2 border-blue-200' : ''}`}
+            onClick={openFileDialog}
+            onDragOver={(e) => dragOverClass(e, true)}
+            onDragLeave={(e) => dragOverClass(e, false)}
+            onDrop={handleDrop}
+        >
+            {/* Tampilkan Placeholder atau Pratinjau Gambar */}
+            {!imageUrl ? (
+                <div className="bg-slate-200 w-full h-full rounded-full flex justify-center items-center">
+                    <User size={30} />
+                </div>
+            ) : (
+                <img
+                    src={imageUrl}
+                    alt="Avatar Preview"
+                    className="w-full h-full rounded-full object-cover"
+                />
+            )}
+
+            <input
+                type="file"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                accept="image/*"
+                name="avatar"
+                className="hidden"
+            />
+        </div>
+  )
+}
+
+export default AvatarUpload;
