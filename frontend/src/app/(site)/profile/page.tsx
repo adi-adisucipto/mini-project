b23ba7a75106c58fa867bae217d4fcb75c4d0501
adi-adisucipto <suciptoadi550@gmail.com>
2025-12-02 13:25:40 +0700

    Purchasing UI (#23)
    * Finish Purchasing with UI
    
    * Purchasing Mobile UI
    
    * Edited
    
    * Profile Page with UI

--- frontend/src/app/(site)/profile/page.tsx
+++ frontend/src/app/(site)/profile/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import Button from "@/components/Button";
+import Container from "@/components/Container";
 import axios from "axios";
+import { User } from "lucide-react";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 interface UserData {
@@ -27,12 +31,12 @@ function Profile() {
       if(status === "authenticated" && session) {
         const user = async () => {
             try {
-              const userInfo = await axios.post(`http://localhost:8000/api/user/profile`, 
+              const userInfo = await axios.post(`http://localhost:8000/api/profile/profile`, 
                 {
                   email: email
                 },
               )
-              setData(userInfo.data.user)
+              setData(userInfo.data.user);
             } catch (error) {
               console.log(error);
             }
@@ -42,20 +46,83 @@ function Profile() {
       }
     }, [session, status])
   return (
-    <div className="flex flex-col justify-center items-center h-[85vh]">
-      <div className="w-4xl max-w-4xl h-[80%] bg-slate-100 rounded-2xl shadow-2xl p-12 grid grid-cols-2 grid-rows-2 gap-5">
-        <div className="row-span-2 p-5 bg-amber-100 rounded-lg shadow-lg flex flex-col gap-3">
-          <img src={data?.avatar} className="h-[50%] w-full object-cover rounded-2xl"/>
-          <h1 className="text-2xl font-bold text-center">{data?.username}</h1>
-          <div>
-            <p>Email: {data?.email}</p>
-            <p>Referral Code: {data?.referral_code}</p>
-            <p>Points: {data?.points_balance}</p>
+    <div className="w-full lg:h-[85vh] flex items-center justify-center">
+      <Container className="my-10 lg:my-0">
+        {/* Judul */}
+        <div className="lg:flex lg:justify-between justify-center items-center lg:mx-[30px] lg:mt-[30px] lg:mb-[46px]">
+          <div className="flex lg:flex-row flex-col justify-center items-center">
+            <div className="w-20 h-20 mr-[25px] relative">
+              {data?.avatar ? (
+                <Image src={data.avatar} fill={true} alt="avatar" className="w-20 h-20 rounded-full absolute object-cover"/>
+              ) : (
+                <div className="w-20 h-20 rounded-full bg-slate-200 flex items-center justify-center">
+                  <User size={40}/>
+                </div>
+              )}
+            </div>
+
+            <div className="flex flex-col lg:gap-2.5 gap-[5px]">
+              <div className="text-white lg:text-[25px] text-[20px] font-medium tracking-widest mx-auto lg:mx-0">{data?.username}</div>
+              <div className="text-white/70 lg:text-[18px] text-[15px] font-medium tracking-widest mx-auto lg:mx-0">{data?.email}</div>
+            </div>
+          </div>
+
+          <div className="w-[100px] lg:flex hidden">
+            <Button>Edit</Button>
           </div>
         </div>
-        <div className="p-5 bg-amber-50 rounded-lg shadow-lg">Adi</div>
-        <div className="p-5 bg-amber-100 rounded-lg shadow-lg">Adi</div>
-      </div>
+
+        {/* Info Account */}
+        <div className="lg:px-[30px] lg:flex w-full gap-20 mb-[30px] mt-[30px] lg:mt-0">
+          <div className="lg:w-[50%] flex flex-col gap-[25px] mb-[25px] lg:mb-0">
+            <div className="">
+              <h1 className="text-white text-[18px] font-medium tracking-widest">Username</h1>
+              <div className="h-10 text-white bg-white/30 rounded-[10px] px-5 flex items-center">
+                {data?.username}
+              </div>
+            </div>
+
+            <div className="">
+              <h1 className="text-white text-[18px] font-medium tracking-widest">Gender</h1>
+              <div className="h-10 text-white bg-white/30 rounded-[10px] px-5 flex items-center">
+                
+              </div>
+            </div>
+
+            <div className="">
+              <h1 className="text-white text-[18px] font-medium tracking-widest">Referral Code</h1>
+              <div className="h-10 text-white bg-white/30 rounded-[10px] px-5 flex items-center">
+                {data?.referral_code}
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:w-[50%] flex flex-col gap-[25px]">
+            <div className="">
+              <h1 className="text-white text-[18px] font-medium tracking-widest">Email</h1>
+              <div className="h-10 text-white bg-white/30 rounded-[10px] px-5 flex items-center">
+                {data?.email}
+              </div>
+            </div>
+
+            <div className="">
+              <h1 className="text-white text-[18px] font-medium tracking-widest">Phone Number</h1>
+              <div className="h-10 text-white bg-white/30 rounded-[10px] px-5 flex items-center"></div>
+            </div>
+
+            <div className="">
+              <h1 className="text-white text-[18px] font-medium tracking-widest">Points</h1>
+              <div className="h-10 text-white bg-white/30 rounded-[10px] px-5 flex items-center">
+                {data?.points_balance}
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+
+        <Button className="lg:hidden flex justify-center items-center">Edit</Button>
+      </Container>
     </div>
   )
 }
