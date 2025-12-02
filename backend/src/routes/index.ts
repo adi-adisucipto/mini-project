c5b76e6f434b3c9b825bcf15a0ad136a8b7cac8f
adi-adisucipto <suciptoadi550@gmail.com>
2025-12-02 13:25:40 +0700

    Purchasing UI (#23)
    * Finish Purchasing with UI
    
    * Purchasing Mobile UI
    
    * Edited
    
    * Profile Page with UI

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth.router";
 import purchasingAuth from "./purchasing.router";
 import eoTransactionRouter from "./eoTransaction.router";// routes/index.ts
 import eoEventsRouter from "./eoEvents.router";
+import profileRouter from "./profile.router";
 
 
 const router = Router();
@@ -11,5 +12,6 @@ router.use("/auth", authRouter);
 router.use("/transaction", purchasingAuth);
 router.use("/eo/transactions", eoTransactionRouter);
 router.use("/eo/events", eoEventsRouter);
+router.use("/profile", profileRouter)
 
 export default router;
